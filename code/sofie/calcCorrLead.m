0dcd5ee1a5bcee0ff665fa01be2c8aa3df091904
Sofie Therese <sofha@dtu.dk>
2015-07-24 14:38:21 +0200

    add alternative px_est

--- code/sofie/calcCorrLead.m
+++ code/sofie/calcCorrLead.m
@@ -1,4 +1,4 @@
-function [corr_TE, corr_TR] = calcCorrLead(Cxxe,Y,wx,wy,wt,idx,tr_idx)
+function [corr_TE, corr_TR] = calcCorrLead(Xe,Cxxe,Y,wx,wy,wt,idx,tr_idx)
 % Inputs:
 %   idx - validation set indices
 Nt = length(wt);
@@ -25,12 +25,25 @@ px_est = px_est-mean(px_est);
 wt_tmp = wt/sum(wt);
 px_flt_est = filter(wt_tmp, 1, px_est);
 
+
+keyboard
+% keyboard
+% %%
+px_est2 = zeros(1,Ne);
+for e=1:Ne
+    px_est2(e) = var(Xe(:,:,e) * wx);
+end
+% - filter X source with temporal filter (should mimic hrf)
+%wt_tmp = wt/sum(wt);
+px_flt_est2 = filter(wt_tmp, 1, px_est2);
+%%
+%keyboard
 % corr temp, est filt sx and sy_est, test
-corrtmp = corrcoef(sy_est(val_idx), px_flt_est(val_idx));
+corrtmp = corrcoef(sy_est(val_idx), px_flt_est2(val_idx));
 corr_TE = (corrtmp(1,2));
 
 % corr temp, est filt sx and sy_est, train
-corrtmp = corrcoef(sy_est(tr_idx), px_flt_est(tr_idx));
+corrtmp = corrcoef(sy_est(tr_idx), px_flt_est2(tr_idx));
 corr_TR = (corrtmp(1,2));
 
 
